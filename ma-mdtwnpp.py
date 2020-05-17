0ec185cd384b984afed21e900c5e9eaca085cb44
luisbalru <balderasruizluis@gmail.com>
2020-05-17 10:47:27 +0200

    creada función de fitness

--- ma-mdtwnpp.py
+++ ma-mdtwnpp.py
@@ -12,6 +12,13 @@ def lecturaDatos(nombre_archivo):
     return(data.to_numpy())
 
 
+def fitness(clase0,clase1,data):
+    df0 = data[clase0,:]
+    df1 = data[clase1,:]
+    suma0 = np.sum(df0,axis=0)
+    suma1 = np.sum(df1,axis=0)
+    return(max(abs(suma0-suma1)))
+
 def MA_MDTWNPP(nombre_archivo, generaciones, n_poblacion, descendientes):
     data = lecturaDatos(nombre_archivo)
-    poblacion = generaPoblacion(n_poblacion)
+    poblacion = generaPoblacion(data,n_poblacion)
